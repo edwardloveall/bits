e3ffbf30d66cffe6a8df6972d8e1ad11a9bf4398
Edward Loveall <edward@edwarloveall.com>
2012-03-03 08:08:44 -0500

    Fixed auth_token bug
    generate_token no longer looks for a column in the users table named
    "column"
    Added a seed to create a user when there is none

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,6 @@ class User < ActiveRecord::Base
   def generate_token(column)
     begin
       self[column] = SecureRandom.urlsafe_base64
-    end while User.exists(column: self[column])
+    end while User.exists?(column => self[column])
   end
 end
