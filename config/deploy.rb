d93ff62b40332f52a7fcac027efe48cbdac582e8
Edward Loveall <edward@edwarloveall.com>
2012-04-23 09:41:06 -0400

    Slight change for deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,7 +9,7 @@ set :deploy_via, :remote_cache
 set :use_sudo, false
 
 set :scm, "git"
-set :repository, "git@github.com:edwardloveall/bits.git"
+set :repository, "git@github.com:edwardloveall/#{application}.git"
 set :branch, "master"
 
 default_run_options[:pty] = true
